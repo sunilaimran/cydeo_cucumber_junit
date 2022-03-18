fc690bda9a6e2c8ac9470f5920a968f9879faf4b
sunila <imransunila@yahoo.com>
2022-03-18 11:35:42 -0400

    Driver.class

--- src/test/java/com/cydeo/uitilies/Driver.java
+++ src/test/java/com/cydeo/uitilies/Driver.java
@@ -8,19 +8,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
-     /*
-    Creating a private constructor, so we are closing
-    access to the object of this class from outside the class
-     */
-
+    /*
+     Creating a private constructor, so we are closing
+     access to the object of this class from outside the class
+      */
     private Driver(){}
 
     /*
-We make WebDriver private, because we want to close access from outside the class.
-We make it static because we will use it in a static method.
- */
-    private static WebDriver driver;
-
+    We make WebDriver private, because we want to close access from outside the class.
+    We make it static because we will use it in a static method.
+     */
+    private static WebDriver driver; // value is null by default
 
     /*
     Create a re-usable utility method which will return same driver instance when we call it
@@ -28,17 +26,20 @@ We make it static because we will use it in a static method.
     public static WebDriver getDriver(){
 
         if (driver == null){
-                   /*
+
+            /*
             We read our browserType from configuration.properties.
             This way, we can control which browser is opened from outside our code, from configuration.properties.
              */
             String browserType = ConfigurationReader.getProperty("browser");
-           /*
+
+
+            /*
                 Depending on the browserType that will be return from configuration.properties file
                 switch statement will determine the case, and open the matching browser
             */
             switch (browserType){
-                case  "chrome":
+                case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                     driver.manage().window().maximize();
@@ -48,11 +49,12 @@ We make it static because we will use it in a static method.
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                     driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
-            }
 
+            }
         }
+
         return driver;
 
     }
@@ -62,9 +64,9 @@ We make it static because we will use it in a static method.
      */
     public static void closeDriver(){
         if (driver != null){
-            driver.quit();//this line will terminate the existing session. value will not even be null
+            driver.quit(); // this line will terminate the existing session. value will not even be null
             driver = null;
         }
-
     }
+
 }
