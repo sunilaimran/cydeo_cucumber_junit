aab3bdbd53e32da917ba6896bd46bb99b3ca43bc
sunila <imransunila@yahoo.com>
2022-03-18 11:37:08 -0400

    CukesRunner.java

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@employee and not @admin"
+        tags = "@wip"
 
 )
 public class CukesRunner {
