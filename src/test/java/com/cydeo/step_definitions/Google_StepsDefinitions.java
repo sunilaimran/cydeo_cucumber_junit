8a1901cd1220255a89b96282ea7df7550769f4cd
sunila <imransunila@yahoo.com>
2022-03-21 17:10:15 -0400

    Google_StepsDefinitions.java

--- src/test/java/com/cydeo/step_definitions/Google_StepsDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Google_StepsDefinitions.java
@@ -1,27 +1,47 @@
 package com.cydeo.step_definitions;
 
 import com.cydeo.pages.GoogleSearchPage;
-import com.cydeo.uitilies.Driver;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.Keys;
 
-public class GoogleStepsDefinitions {
+public class Google_StepsDefinitions {
 
    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
-
     @When("user types apple and clicks enter")
-    public void user_types_apple_and_clicks_enter() {
+    public void user_types_and_clicks_enter2() {
         googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
+
+
+    @When("user types {string} and clicks enter")
+    public void user_types_and_clicks_enter(String string) {
+        googleSearchPage.searchBox.sendKeys(string + Keys.ENTER);
+    }
+
+
+    @Then("user sees {string} in the google title")
+    public void user_sees_in_the_google_title(String string) {
+
+        String expectedTitle = string+" - Google Search";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        //junit assertion accept first arg as expected, second arg as actual
+        Assert.assertEquals("Title is not expected!", expectedTitle,actualTitle);
+        //Assert.assertTrue(actualTitle.equals(expectedTitle));
+
+    }
+
+
     @Then("user sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
 
-         String expectedTitle = "apple - Google search";
+         String expectedTitle = "apple - Google Search";
          String actualTitle = Driver.getDriver().getTitle();
-        //junit assertion accpect first arg as expected, second arg as actual
+        //junit assertion accept first arg as expected, second arg as actual
          Assert.assertEquals("Title is not expected!", expectedTitle,actualTitle);
          //Assert.assertTrue(actualTitle.equals(expectedTitle));
     }
@@ -38,7 +58,7 @@ public class GoogleStepsDefinitions {
         String expectedTitle = "Google";
         String actualTitle = Driver.getDriver().getTitle();
 
-        Assert.assertEquals(actualTitle, expectedTitle);
+       Assert.assertEquals(actualTitle, expectedTitle);
 
 
 
