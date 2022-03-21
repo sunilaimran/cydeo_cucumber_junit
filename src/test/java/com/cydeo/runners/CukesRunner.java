40db1a06ddf99ac3a66ab86518d92b564b6ab34e
sunila <imransunila@yahoo.com>
2022-03-21 13:04:12 -0400

    CukesRunner.java

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",
-        features = "src/test/resources/features",
+        plugin = "html:target/cucumber-report.html",//determines what type of report we want to generate with our project, and also where we want to store
+        features = "src/test/resources/features",//we provide the path of the 'feature' directory and let
         glue = "com/cydeo/step_definitions",
         dryRun = false,
         tags = "@wip"
